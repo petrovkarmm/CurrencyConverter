50429af72a88a4d3188077882a9aaf1f9e575615
petrovkarmm <petrovleonid1999@mail.ru>
2023-09-15 01:56:32 +0500

    add app_name

--- currencyConverter/app_converter/urls.py
+++ currencyConverter/app_converter/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import get_currency_rate
 
+app_name = 'app_converter'
+
 urlpatterns = [
     path('api/rates/', get_currency_rate, name='get_currency_rate'),
 ]
